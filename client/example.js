befccf13bf4d699386d30e7536ad6d335797829f
Jeff Mesnil <jmesnil@gmail.com>
2010-01-21 10:57:02 +0100

    improved stomp object construction

--- client/example.js
+++ client/example.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
+
   $('#connect_form').submit(function() {
     url = $("#connect_url").val();
     login = $("#connect_login").val();
     passcode = $("#connect_passcode").val();
     
-    client = new Stomp(url);
-    client.receive = function(message)
-    {
+    var client = stomp(url);
+    client.onreceive = function(message) {
       debug(">>> got message " + message);
     }
+    client.onconnect = function() {
+      debug(">>> [CONNECTED] to Stomp ");
+    };
+    client.ondisconnect = function() {
+      debug(">>> [DISCONNECTED] to Stomp ");
+    };
+
     client.connect(login, passcode);
-    //client.onmessage("wtf");
+    
+    // FIXME simutate openging the web socket
+    client.onopen();
+    // FIXME simutate receiving a message
+    client.onmessage("wtf");
+    
+    client.disconnect();
+
     return false;
   });
-  
 });
--- client/stomp.js
+++ client/stomp.js
@@ -1,56 +1,78 @@
+// client can implement the eventhandlers:
+// * onconnect    => to be notified when it is connected to the STOMP server
+// * ondisconnect => to be notified when it is disconnected from the STOMP server
+// * onreceive    => to receive STOMP messages
+// * onreceipt    => to receive STOMP receipts
+// * onerror      => to receive STOMP errors
 
-Stomp = function (url){
+var stomp = function (url){
 
+  var client, ws, login, passcode;
+  
   debug = function(str) {
     $("#debug").append("<p>"+str+"</p>");
   }
 
-  return {
-    ws: null,
-    login: null,
-    passcode: null,
+  onclose = function()
+  {
+    debug("Whoops! Looks like you lost internet connection or the server went down");
+  };
 
-    connect : function(login, passcode) {
-      debug("Opening Web Socket...");
-      this.ws = new WebSocket(url);
-      this.ws.onmessage = this.onmessage;
-      this.ws.onclose   = this.onclose;
-      this.ws.onopen    = this.onopen;
-      this.login = login;
-      this.passcode = passcode;
-    },
+  onopen = function() {
+    debug('Web Socket Opened...');
+    debug("[CONNECT]: " + login + " " + passcode);
+    // send to the server a CONNECT frame
+    // FIXME this should go in onmessage when receiving a CONNECTED:
+    if (client.onconnect) {
+      client.onconnect();
+    }
+  };
 
-    onclose: function()
-    {
-      debug("Whoops! Looks like you lost internet connection or the server went down");
-    },
+  onmessage = function(evt) {
+    debug('[RECEIVE] ' + evt);
+    // next, check what type of message RECEIPT, ERROR, CONNECTED, RECEIVE
+    // and create appropriate js objects and calls handler for received messags
+    // when CONNECTED is received, call onconnect
+    // when RECEIVE is received, call onreceive
+    // when ERROR is received, call onerror
+    // when RECEIPT is received, call onreceipt
+    if (client.onreceive) {
+      client.onreceive(evt);
+    }
+  };
 
-    onopen : function() {
-      debug('Web Socket Opened...');
-      debug("[CONNECT]: " + login + " " + password);
-      // send to the server a CONNECT frame
-    },
+  client = {};
 
-    disconnect : function()
-    {
-      debug("[DISCONNECT]");
-      // send to the server a DISCONNECT frame
-      debug("Disconnecting...");
-      this.ws.close();
-    },
+  client.connect = function(login_, passcode_) {
+    debug("Opening Web Socket...");
+    ws = new WebSocket(url);
+    ws.onmessage = onmessage;
+    ws.onclose   = onclose;
+    ws.onopen    = onopen;
+    login = login_;
+    passcode = passcode_;
+  };
 
-    onmessage : function(evt) {
-      debug('[RECEIVE] ' + evt);
-      // next, check what type of message RECEIPT, ERROR, CONNECTED, RECEIVE
-      // and create appropriate js objects and calls handler for received messags
-      this.receive(evt);
-    },
+  client.disconnect = function() {
+    debug("[DISCONNECT]");
+    // send to the server a DISCONNECT frame
+    debug("Disconnecting...");
+    ws.close();
+    if (client.ondisconnect) {
+      client.ondisconnect();
+    }
+  };
 
-    send : function(headers, body) {
-      debug("[SEND] " + headers + " " + body);
-      // send to the server
-      // dataStr = "...."
-      // Stomp.ws.send(dataStr);
-    },
-  }
+  client.send = function(headers, body) {
+    debug("[SEND] " + headers + " " + body);
+    // send to the server
+    // dataStr = "...."
+    // Stomp.ws.send(dataStr);
+  };
+
+  // FIXME temporary exposes the handlers to simulate events
+  client.onmessage = onmessage;
+  client.onopen = onopen;
+
+  return client;
 };
