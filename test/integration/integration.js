25bbeedd160789d1049becdc070600cd282e34c6
Jeff Mesnil <jmesnil@gmail.com>
2010-01-26 19:37:14 +0100

    add integration test

---
+++ test/integration/integration.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+  var url = "ws://localhost:61613/stomp";
+  var login = "brianm";
+  var passcode = "wombats"
+  var destination = "/queue/test";
+  
+  $("#server_url").text(url);
+  $("#queue_name").text(destination);
+  
+  var client = Stomp.client(url);
+
+  debug = function(str) {
+    $("#debug").append(str + "\n");
+  };
+  client.debug = debug;
+  // the client is notified when it is connected to the server.
+  client.onconnect = function(frame) {
+      debug("connected to Stomp");
+      client.subscribe(destination, "auto", {receipt: 123});
+  };
+  client.onerror = function(frame) {
+      debug("connected to Stomp");
+  };
+  client.ondisconnect = function() {
+    debug("disconnected from Stomp");
+  };
+  client.onreceipt = function() {
+    debug("receipt from Stomp");
+    client.send(destination, {foo: 1}, "test")
+  };
+  client.onreceive = function(message) {
+    client.disconnect();
+  };
+
+  client.connect(login, passcode);
+
+  return false;
+});
