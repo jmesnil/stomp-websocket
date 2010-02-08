401caf62792bffd7807f4cf5bc08d543ac13e683
Jeff Mesnil <jmesnil@gmail.com>
2010-02-08 12:45:37 +0100

    test: message with JSON stringified body

--- test/unit/message.js
+++ test/unit/message.js
@@ -17,3 +17,25 @@ test("Send and receive a message", function() {
     });
     stop(TEST.timeout);
 });
+
+test("Send and receive a message with a JSON body", function() {
+  
+  var client = Stomp.client(TEST.url);
+  var payload = {text: "hello", bool: true, value: 1.234};
+  
+  client.connect(TEST.login, TEST.password,
+    function() {
+      client.subscribe(TEST.destination, function(message)
+      {
+        start();
+        var res = JSON.parse(message.body);
+        equals(res.text, payload.text);
+        equals(res.bool, payload.bool);
+        equals(res.value, payload.value);
+        client.disconnect();
+      });
+      
+      client.send(TEST.destination, {}, JSON.stringify(payload));
+    });
+    stop(TEST.timeout);
+});
