71271fede9601f2f24e1edc90236f9376a05cd1f
Jeff Mesnil <jmesnil@gmail.com>
2010-04-08 18:11:00 +0200

    tests
    * use Math.random() to generate message body

--- test/unit/transaction.js
+++ test/unit/transaction.js
@@ -4,6 +4,7 @@ test("Send a message in a transaction and abort", function() {
   
   var txid = "txid_" + Math.random();
   var body = Math.random();
+  var body2 = Math.random();
   
   var client = Stomp.client(TEST.url);
   
@@ -14,14 +15,14 @@ test("Send a message in a transaction and abort", function() {
       {
         start();
         // we should receive the 2nd message outside the transaction
-        equals(message.body, "outside");
+        equals(message.body, body2);
         client.disconnect();
       });
       
       client.begin(txid);
       client.send(TEST.destination, {transaction: txid}, body);
       client.abort(txid);
-      client.send(TEST.destination, {}, "outside");
+      client.send(TEST.destination, {}, body2);
     });
     stop(TEST.timeout);
 });
