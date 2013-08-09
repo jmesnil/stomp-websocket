614081371c8dff4e3433a9033a97ca4aeb2b72f1
Jeff Mesnil <jmesnil@gmail.com>
2013-08-09 17:49:26 +0200

    revert wrong changes to example's server port

--- example/server.js
+++ example/server.js
@@ -4,5 +4,5 @@ connect()
   .use(connect.logger('dev'))
   .use(connect.static(__dirname))
   .use(connect.static(__dirname + '/../dist/'))
-  .listen(8081);
-console.log('Server running at http://0.0.0.0:8081/');
+  .listen(8080);
+console.log('Server running at http://0.0.0.0:8080/');
